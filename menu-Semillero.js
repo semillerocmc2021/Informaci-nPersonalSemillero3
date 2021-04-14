36be80e266a624e40a2b3262ab88b2613d94905e
Pedronet1997 <pedro_guale@outlook.com>
2021-04-14 12:01:39 -0500

    Se agregan menus PGG

---
+++ menu-Semillero.js
@@ -0,0 +1,20 @@
+{
+    "COMMONS": {
+        "MENU": {
+			"MNU_SMCMC":"Formularios Semillero",
+            "MNU_PGG_BVDA":"Bienvenida Pedro Guale",
+			"MNU_PGG_CAL":"Calificaciones Pedro Guale",
+			"MNU_PGG_CALC":"Calculadora Pedro Guale",
+			"MNU_PGG_EVA2":"Evaluacion 2 Pedro Guale",
+			"MNU_PGG_BliCli":"Bli Crear Cliente Pedro Guale",
+			"MNU_PGG_BliCom":"Bli Crear Comercial Pedro Guale",
+			"MNU_PGG_BusCli":"Buscar Cliente Pedro Guale",
+			"MNU_PGG_GridCli":"Grid Cliente Pedro Guale",
+			"MNU_PGG_BusCli2":"Busqueda Cliente Pedro Guale",
+			"MNU_EVALUACION":"Evaluacion 2",
+			"Pedro_Guale":"Menu PCGG",
+			"MNU_PGG_RegProd":"Registrar Producto PGG",
+			"MNU_PGG_GridProd":"Grid Productos PGG"
+        }
+    }
+}
