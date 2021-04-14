968528aecdf1249cc10efc4cb851d117f69c1a35
carSneider99 <carsneider99@gmail.com>
2021-04-14 12:01:58 -0500

    version inicial menu semillero

---
+++ menu-semillero.js
@@ -0,0 +1,11 @@
+{
+    "COMMONS": {
+        "MENU.MNU_CSSS_BV":"BienvenidaCarlosSoto",
+		"MENU.MNU_SMCMC":"FormulariosSemillero",
+		"MENU.MNU_CSSS_CAL":"CalificacionesCarlosSoto",
+		"MENU.MNU_CSSS_CALCU":"CalculadoraCarlosSoto",
+		"MENU.MNU_CSSS_Evaluacion2":"Evaluacion2CarlosSoto",
+		"MENU.MNU_CSSS_RegCliente" : "Registrar Cliente",
+		"MENU.MNU_CSSS_EVA4" : "Buscar clientes CSSS"
+    }
+}
